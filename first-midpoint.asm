0f8c5938307e37e0ad811b95dabade66996d930a
Henrik Zenkert <henrik.zenkert@gmail.com>
2023-03-03 17:13:09 +0100

    First part done, 7.34

---
+++ first-midpoint.asm
@@ -0,0 +1,22 @@
+.ORIG x3000
+    AND R0, R0, x0
+    ADD R1, R0, x5
+    ST R0, A
+    ST R1, B
+    AND R0, R0, x0
+    AND R1, R0, x0
+    LD R0, A
+    LD R1, B
+X   NOT R2, R0
+    ADD R2, R2, x1
+    ADD R2, R2, R1
+    BRnz DNE
+    ADD R1, R1, x-1
+    ADD R0, R0, x1
+    BR X
+DNE ST R1, C
+    HALT
+A   .BLKW 1
+B   .BLKW 1
+C   .BLKW 1
+    .END
