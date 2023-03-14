d34637ff0a042484377501bafff93f5ce36db9f6
Henrik Zenkert <henrik.zenkert@gmail.com>
2023-03-14 19:37:24 +0100

    optional 1 is working now.

--- 1st-optional.asm
+++ 1st-optional.asm
@@ -7,7 +7,10 @@ LOOP
     JSR resultS
     JSR LOOP
 	HALT
+MSG-INPUT   .STRINGZ "Input decimal number with 1-5 digits. End with newline:"
+.END
 
+.ORIG x4000
     ;; Checks if number is prime
     ;; If number is prime R0 is set to 1, else 0
 isPrime
@@ -80,7 +83,7 @@ SKIP
     RET
 
 ;;; MESSAGES
-MSG-INPUT           .STRINGZ "Input a 2 digit decimal number:"
+
 MSG-IS-PRIME        .STRINGZ "The number is prime.\n"
 MSG-IS-NOT-PRIME    .STRINGZ "The number is not prime.\n"
 
@@ -90,9 +93,6 @@ SAVE2REG2       .BLKW 1
 SAVE2REG3       .BLKW 1
 SAVE2REG4       .BLKW 1
 SAVE2REG5       .BLKW 1
-.END
-;; read in two digit number
-.ORIG x4000
 
 ;; Puts result in R4 and quotient in R5
 ;; Input is R0, number to divide and R1, divisor
@@ -116,68 +116,88 @@ SUB     ADD R4, R4, x1
         LD R2, SAVEREG3
         RET
 ;; Recieves two digit number and saves it to R0
+
 readS
-    ST R1, SAVEREG1 ;; save all the registers we use.
+    
+    ST R1, SAVEREG1 ;; save registers we will use.
     ST R2, SAVEREG2
     ST R3, SAVEREG3
     ST R4, SAVEREG4
+    ST R5, SAVEREG5
+    ST R7, SAVEREG7
     
-    AND R2, R2, x0
-	ADD R1, R2, x5
-    LD R2, CVT
-    LEA R4, DIGITS
-    
-GETNUM	
-    GETC
-    OUT
-    ADD R3, R0, x-A ;; If newline, we stop saving numbers
-    BRz LEAVE
-    ADD R0, R0, R2
-    STR R0, R4, x0
-    ADD R4, R4, x1 
-    ADD R1, R1, x-1 ;; After 5 digits we leave
-    BRz LEAVE
-	BRnzp GETNUM
-LEAVE
-    ;Save R0 in R4 to output new line char
-    AND R0, R0, x0 
-    ADD R0, R0, xA ;; Newline character
-    PUTC ;; Print newline
-    
-    ADD R3, R3, x0
-    ADD R3, R3, x5 ;; we use r3, to loop 5 times in LOP
-    ADD R1, R3, x1 ;; R1 is times we have number times 10, When loop finishes 0 for digit 0, 1 for digit 1... 
-    ADD R2, R3, x0 ;; R2 is number of times we should multiply current number by 10
-    
-AGAIN
-    AND R5, R5, x0
-    LDR R0, R4, x0
-    ADD R4, R4, x-1
-    ADD R1, R1, x-1
-    BRz 
 
-    ADD R5, R5, R1 ;; Multiply 10 value by 5, then double to turn 2 -> 20, 3 -> 30 usw
-    ADD R5, R5, R1
-    ADD R3, R3, x-1
-    BRp TIMES-5
-    ADD R2, R2, R2
-    ADD R0, R2, R0
+    AND R1, R1, x0
+    LD R3, NEWLINE
+    NOT R3, R3
+    ADD R3, R3, x1 ;; Use R3 to check if char is newline
+    ADD R1, R1, x5 ;; Loop counter
+    LD R4, ASCII-TO-NUM ;; Conversion
+    LEA R5, DIGITS      ;; Pointer to where we save DIGITS
+GETNUM
+    GETC
+    ADD R2, R0, R3
+    BRz LEAVE
+    OUT
+    ADD R0, R0, R4  ; Convert ascii to numeric
+    STR R0, R5, x0  ; Store digit
+    ADD R5, R5, x1  ; Move pointer to DIGITS one.
+    ADD R1, R1, x-1 
+    BRp GETNUM
+LEAVE
+    ;; We now have 5 digits in DIGITS
+    NOT R1, R1
+    ADD R1, R1, x1
+    ADD R1, R1, x5
+    ;; Print a newline
+    LD R0, NEWLINE
+    OUT
+    ;; R1 contains number of digits we have recieved
+    LEA R5, DIGITS
+    AND R0, R0, x0
+FOR-EACH-DIGIT
+    LDR R3, R5, x0
+    ADD R2, R1, x-1
+FOR-SIZE-OF-DIGIT
+    BRnz LEAVE-INNER-DIGIT-LOOP
+    JSR MULTIPLY-BY-TEN
+    ADD R2, R2, x-1
+    BRnzp FOR-SIZE-OF-DIGIT
+LEAVE-INNER-DIGIT-LOOP
+    ADD R0, R0, R3
+    ADD R5, R5, x1
+    ADD R1, R1, x-1
+    BRp FOR-EACH-DIGIT
+
     LD R1, SAVEREG1 ;; restore registers we used.
     LD R2, SAVEREG2
     LD R3, SAVEREG3
-    LD R3, SAVEREG4
+    LD R4, SAVEREG4
+    LD R5, SAVEREG5
+    LD R7, SAVEREG7
     RET
 
-DIGIT-DONE 
 
-DONE
+MULTIPLY-BY-TEN ; Multiplies R3 by ten
+    ST R1, REG1SAVE
+    ADD R1, R3, R3
+    ADD R1, R1, R3
+    ADD R1, R1, R3
+    ADD R1, R1, R3
+    ADD R3, R1, R1
+    LD R1, REG1SAVE
+    RET
 
-SAVEREG1    .BLKW 1
-SAVEREG2    .BLKW 1
-SAVEREG3    .BLKW 1
-SAVEREG4    .BLKW 1
 
+SAVEREG1        .BLKW 1
+SAVEREG2        .BLKW 1
+SAVEREG3        .BLKW 1
+SAVEREG4        .BLKW 1
+SAVEREG5        .BLKW 1
+SAVEREG7        .BLKW 1
 DIGITS .BLKW 5
+ASCII-TO-NUM    .FILL #-48
+REG1SAVE        .BLKW 1
+NEWLINE         .FILL xA
 
-CVT .FILL #-48
-    .END
+.END
